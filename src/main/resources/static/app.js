f4c61bdf0bc6412ccc20480934e79145b382baa9
emiduf <emiliedufseth@hotmail.com>
2025-07-25 16:01:01 +0200

    Nedtrekksliste kategori

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,15 @@
+//Kategorier
+document.addEventListener('DOMContentLoaded', function() {
+    const knapp = document.getElementById('kategorier-knapp');
+    const meny = document.getElementById('kategori-meny');
+
+    knapp.addEventListener('click', function() {
+        meny.style.display = meny.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    //Lukker menyen hvis man klikker utenfor
+    document.addEventListener('click', function (e) {
+        if (!document.getElementById('kategorier').contains(e.target))
+            meny.style.display = 'none';
+    });
+});
