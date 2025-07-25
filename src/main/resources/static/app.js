23e18c1d55f79a4458dbcdd4bf029c694367a6d3
emiduf <emiliedufseth@hotmail.com>
2025-07-25 21:00:02 +0200

    Oppskrifter meny

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,3 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
             meny.style.display = 'none';
     });
 });
+
+//Oppskrifter
+document.addEventListener('DOMContentLoaded', function() {
+    const knapp = document.getElementById('oppskrifter-knapp');
+    const meny = document.getElementById('oppskrift-meny');
+
+    knapp.addEventListener('click', function() {
+        meny.style.display = meny.style.display === 'flex' ? 'none' : 'flex';
+    })
+
+    //Lukker menyen hvis man klikker utenfor
+    document.addEventListener('click', function(e) {
+        if (!document.getElementById('oppskrifter').contains(e.target))
+            meny.style.display = 'none';
+    });
+});
