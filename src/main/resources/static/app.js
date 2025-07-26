6e29be609b871f4085c7e2ed0d618c8f04b45d8b
emiduf <emiliedufseth@hotmail.com>
2025-07-26 19:12:41 +0200

    js kategorier og oppskrifter

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,32 +1,41 @@
-//Kategorier
 document.addEventListener('DOMContentLoaded', function() {
     const katKnapp = document.getElementById('kategorier-knapp');
     const katMeny = document.getElementById('kategori-meny');
-
-    katKnapp.addEventListener('click', function(event) {
-        event.stopPropagation();
-        katMeny.style.display = katMeny.style.display === 'flex' ? 'none' : 'flex';
-    });
-
-    //Lukker menyen hvis man klikker utenfor
-    document.addEventListener('click', function (e) {
-        if (!katKnapp.contains(e.target) && !katMeny.contains(e.target))
-            katMeny.style.display = 'none';
-    });
-});
-
-//Oppskrifter
-document.addEventListener('DOMContentLoaded', function() {
     const oppKnapp = document.getElementById('oppskrifter-knapp');
     const oppMeny = document.getElementById('oppskrift-meny');
 
-    oppKnapp.addEventListener('click', function(event) {
+    //klikk på kategorier
+    katKnapp.addEventListener('click', function (event) {
         event.stopPropagation();
-        oppMeny.style.display = oppMeny.style.display === 'flex' ? 'none' : 'flex';
-    })
 
-    //Lukker menyen hvis man klikker utenfor
+        //lukk oppskriftsmeny om den er åpen
+        if (oppMeny.style.display === 'flex') {
+            oppMeny.style.display = 'none';
+        }
+
+        //toggle kategori-meny
+        katMeny.style.display = katMeny.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    //klikk på oppskrifter
+    oppKnapp.addEventListener('click', function (event) {
+        event.stopPropagation();
+
+        //lukk kategori-meny om den er åpen
+        if (katMeny.style.display === 'flex') {
+            katMeny.style.display = 'none';
+        }
+
+        //toggle oppskrift-meny
+        oppMeny.style.display = oppMeny.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    //klikk utenfor lukker begge menyer
     document.addEventListener('click', function(e) {
+        if (!katKnapp.contains(e.target) && !katMeny.contains(e.target)) {
+            katMeny.style.display = 'none';
+        }
+
         if (!oppKnapp.contains(e.target) && !oppMeny.contains(e.target)) {
             oppMeny.style.display = 'none';
         }
